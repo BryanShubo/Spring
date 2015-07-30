5f5229a4505218ff13af1cae9ad5c9f15d9013cf
Shubo Zhang <bryanshubo@gmail.com>
2015-07-30 00:10:58 -0400

    chapter_03

--- Chapter_01/knight/src/test/java/sia/knights/BraveKnightTest.java
+++ Chapter_01/knight/src/test/java/sia/knights/BraveKnightTest.java
@@ -8,12 +8,20 @@ import sia.knights.Quest;
 
 public class BraveKnightTest {
 
-  @Test
+ /* @Test
   public void knightShouldEmbarkOnQuest() {
     Quest mockQuest = mock(Quest.class);
     BraveKnight knight = new BraveKnight(mockQuest);
     knight.embarkOnQuest();
     verify(mockQuest, times(1)).embark();//Assert that the quest’s embark() method is called when the knight’s embarkOnQuest() method is called.
-  }
+  }*/
+
+    @Test
+    public void testBraveKnight() {
+        Quest mockQuest = mock(Quest.class);
+        BraveKnight braveKnight = new BraveKnight(mockQuest);
+        braveKnight.embarkOnQuest();
+        verify(mockQuest, times(1)).embark();
+    }
 
 }
